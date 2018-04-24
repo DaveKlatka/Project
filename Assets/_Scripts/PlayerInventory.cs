3d5a149c8652c3e1c078e6429ffe20ccba143837
DaveKlatka <38696743+DaveKlatka@users.noreply.github.com>
2018-04-24 15:19:45 -0400

    Open Dnd 0.001.002

---
+++ Assets/_Scripts/PlayerInventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+    public GameObject thePlayer;
+    public GameObject inventory;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update ()
+    {
+	   if (Input.GetButtonDown("Inventory"))
+        {
+            StartCoroutine(InventoryScreen());
+        }
+	}
+
+    private IEnumerator InventoryScreen()
+    {
+        if (inventory.activeInHierarchy == false)
+        {
+            inventory.SetActive(true);
+            thePlayer.SetActive(false);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+
+            inventory.SetActive(false);
+            thePlayer.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        yield return null;
+    }
+}
